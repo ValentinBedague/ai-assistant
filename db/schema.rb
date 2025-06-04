2309c5e2e6147b31161bce14b66a421d69437389
Valentin Bedague <valentin.bdg@gmail.com>
2025-06-04 16:35:51 +0200

    added images to card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_110440) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_04_141835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_110440) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url_image"
   end
 
   create_table "users", force: :cascade do |t|
