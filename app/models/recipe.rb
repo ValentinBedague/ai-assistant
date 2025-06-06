6bd0ac1fc7bb1eaba2186cbbd2fa583df9e2f0f3
Valentin Bedague <valentin.bdg@gmail.com>
2025-06-06 16:18:13 +0200

    Removing validation Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_one_attached :image
-  
+
   has_many :chats, dependent: :destroy
 
-  validates :name, presence: true
-  validates :portions, presence: true
-  validates :ingredients, presence: true
-  validates :description, presence: true
 end
